1505500e16f53400d2e037b15af6633930d28b92
Артем Холод <artsiom.kholodok@gmail.com>
2020-01-04 08:23:33 +0300

    Initial Commit

---
+++ SushiExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SushiExample
+//
+//  Created by Macbook on 04.01.2020.
+//  Copyright © 2020 EPLAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
