45b8c3bb2f63bbe03316c09a5980f89f020e63bd
Артем Холод <artsiom.kholodok@gmail.com>
2020-01-06 14:00:12 +0300

    menu

---
+++ SushiExample/Controllers/ContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContainerViewController.swift
+//  SushiExample
+//
+//  Created by Macbook on 06.01.2020.
+//  Copyright © 2020 EPLAN. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SushiExample/SupportingFiles/Protocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  SushiExample
+//
+//  Created by Macbook on 06.01.2020.
+//  Copyright © 2020 EPLAN. All rights reserved.
+//
+
+import Foundation
